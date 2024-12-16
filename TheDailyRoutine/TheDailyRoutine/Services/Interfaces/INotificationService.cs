c11689a0426aa523a5713bcc6b8ac64456aa33b5
PetkoDeyanov <petkopetkov1001@gmail.com>
2024-12-16 17:59:11 +0200

    NotificationService Implementation And Interface

---
+++ TheDailyRoutine/TheDailyRoutine/Services/Interfaces/INotificationService.cs
@@ -0,0 +1,13 @@
+﻿using TheDailyRoutine.Entities;
+
+namespace TheDailyRoutine.Services.Interfaces
+{
+    public interface INotificationService
+    {
+        Task<IEnumerable<Notification>> GetUserNotificationsAsync(string userId);
+        Task SendNotificationAsync(Notification notification);
+        Task SendProgressReminderAsync(string userId);
+        Task SendAchievementNotificationAsync(string userId, string message);
+
+    }
+}
